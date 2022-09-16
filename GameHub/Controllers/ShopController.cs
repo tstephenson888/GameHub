5e22b198f9256ed73b5c3fb54121c67e4a1a2dd5
ifotn <rich@infrontofthenet.com>
2022-09-16 13:41:34 -0400

    l2/2 - mock products by category

--- GameHub/Controllers/ShopController.cs
+++ GameHub/Controllers/ShopController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameHub.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameHub.Controllers
 {
@@ -7,6 +8,30 @@ namespace GameHub.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Category(string Name)
+        {
+            // ensure some category name was passed in
+            if (Name == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // get the Name param from the url and store in ViewData object so we can display it
+            ViewData["Category"] = Name;
+
+            // use the Product model to create a list of products in memory for display
+            var products = new List<Product>();
+
+            // use a loop to make some products
+            for (var i = 1; i < 16; i++)
+            {
+                products.Add(new Product { ProductId = i, Name = "Product " + i.ToString(), Price = 10 + i });
+            }
+
+            // load the page and pass it the product data to display
+            return View(products);
         }
     }
 }
