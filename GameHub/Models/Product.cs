2abe0cf71a5a8c1fd407879a91e1c685554d317f
ifotn <rich@infrontofthenet.com>
2022-09-23 11:58:56 -0400

    l3 - scaffold products/create form

--- GameHub/Models/Product.cs
+++ GameHub/Models/Product.cs
@@ -1,9 +1,19 @@
-﻿namespace GameHub.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameHub.Models
 {
     public class Product
     {
         public int ProductId { get; set; }
+
+
+        // validation rules go directly above the property they check
+        [MinLength (2, ErrorMessage = "Hey!  This is way too short!")]
+        [MaxLength (100)]
+        [Required]
         public string? Name { get; set; }
+
+        [Range(0.01, 999999)]
         public decimal? Price { get; set; }
     }
 }
