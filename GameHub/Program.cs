771c86559037c5279ec889bab4b79348ae054b3a
Tyler Stephenson <tylerstephenson888@outlook.com>
2022-11-04 12:19:59 -0400

    Added Google Login to build statement

--- GameHub/Program.cs
+++ GameHub/Program.cs
@@ -15,6 +15,16 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+// enable google auth by reading api keys from appsettings.json or Azure Config section
+var configuration = builder.Configuration;
+
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        options.ClientId = configuration["Authentication:Google:ClientId"];
+        options.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
